30028a3bddc11e1fb1629623592dd1bd4c2b662a
GOKUL <egokul03@gmail.com>
2024-11-21 00:54:32 +0530

    changes

--- layers/backbones/lss_fpn.py
+++ layers/backbones/lss_fpn.py
@@ -268,6 +268,7 @@ class LSSFPN(nn.Module):
             img_neck_conf (dict): Config for image neck.
             depth_net_conf (dict): Config for depth net.
         """
+
         super(LSSFPN, self).__init__()
         self.downsample_factor = downsample_factor
         self.d_bound = d_bound
@@ -306,6 +307,7 @@ class LSSFPN(nn.Module):
 
     def create_frustum(self):
         """Generate frustum"""
+        # make grid in image plane
         ogfH, ogfW = self.final_dim
         fH, fW = ogfH // self.downsample_factor, ogfW // self.downsample_factor
         d_coords = torch.arange(*self.d_bound,
@@ -319,20 +321,42 @@ class LSSFPN(nn.Module):
                                                           1).expand(D, fH, fW)
         paddings = torch.ones_like(d_coords)
 
+        # D x H x W x 3
         frustum = torch.stack((x_coords, y_coords, d_coords, paddings), -1)
         return frustum
 
     def get_geometry(self, sensor2ego_mat, intrin_mat, bda_mat):
-        """Transfer points from camera coord to ego coord."""
+        """Transfer points from camera coord to ego coord.
+
+        Args:
+            rots(Tensor): Rotation matrix from camera to ego.
+            trans(Tensor): Translation matrix from camera to ego.
+            intrins(Tensor): Intrinsic matrix.
+            post_rots_ida(Tensor): Rotation matrix for ida.
+            post_trans_ida(Tensor): Translation matrix for ida
+            post_rot_bda(Tensor): Rotation matrix for bda.
+
+        Returns:
+            Tensors: points ego coord.
+        """
         batch_size, num_cams, _, _ = sensor2ego_mat.shape
+        # undo post-transformation
+        # B x N x D x H x W x 4
         points = self.frustum
         points = points.repeat(batch_size, num_cams, 1, 1, 1, 1).unsqueeze(-1)
+        # cam_to_ego
         points = torch.cat(
             (points[:, :, :, :, :, :2] * points[:, :, :, :, :, 2:3],
              points[:, :, :, :, :, 2:]), 5)
 
         combine = sensor2ego_mat.matmul(torch.inverse(intrin_mat))
-        points = combine.view(batch_size, num_cams, 1, 1, 1, 4, 4).matmul(points)
+        points = combine.view(batch_size, num_cams, 1, 1, 1, 4,
+                              4).matmul(points)
+        # if bda_mat is not None:
+        #     bda_mat = bda_mat.unsqueeze(1).repeat(1, num_cams, 1, 1).view(
+        #         batch_size, num_cams, 1, 1, 1, 4, 4)
+        #     points = (bda_mat @ points).squeeze(-1)
+        # else:
         points = points.squeeze(-1)
         return points[..., :3]
 
@@ -347,7 +371,7 @@ class LSSFPN(nn.Module):
                                       img_feats.shape[1], img_feats.shape[2],
                                       img_feats.shape[3])
         return img_feats
-    
+
     def _forward_depth_net(self, feat, mats_dict):
         return self.depth_net(feat, mats_dict)
 
@@ -360,8 +384,32 @@ class LSSFPN(nn.Module):
                               mats_dict,
                               depth_oracle,
                               is_return_depth=False):
-        """Forward function for single sweep."""
-        batch_size, num_sweeps, num_cams, num_channels, img_height, img_width = sweep_imgs.shape
+        """Forward function for single sweep.
+
+        Args:
+            sweep_index (int): Index of sweeps.
+            sweep_imgs (Tensor): Input images.
+            mats_dict (dict):
+                sensor2ego_mats(Tensor): Transformation matrix from
+                    camera to ego with shape of (B, num_sweeps,
+                    num_cameras, 4, 4).
+                intrin_mats(Tensor): Intrinsic matrix with shape
+                    of (B, num_sweeps, num_cameras, 4, 4).
+                ida_mats(Tensor): Transformation matrix for ida with
+                    shape of (B, num_sweeps, num_cameras, 4, 4).
+                sensor2sensor_mats(Tensor): Transformation matrix
+                    from key frame camera to sweep frame camera with
+                    shape of (B, num_sweeps, num_cameras, 4, 4).
+                bda_mat(Tensor): Rotation matrix for bda with shape
+                    of (B, 4, 4).
+            is_return_depth (bool, optional): Whether to return depth.
+                Default: False.
+
+        Returns:
+            Tensor: BEV feature map.
+        """
+        batch_size, num_sweeps, num_cams, num_channels, img_height, \
+            img_width = sweep_imgs.shape
         img_feats = self.get_cam_feats(sweep_imgs)
         source_features = img_feats[:, 0, ...]
         depth_feature = self._forward_depth_net(
@@ -373,48 +421,49 @@ class LSSFPN(nn.Module):
         )
 
         depth = depth_feature[:, :self.depth_channels].softmax(1)
-        img_feat_with_depth = depth.unsqueeze(
-            1) * depth_feature[:, self.depth_channels:(
-                self.depth_channels + self.output_channels)].unsqueeze(2)
 
-        # Replace voxel_pooling with a fallback (average pooling)
+        depth_feature = torch.stack([kornia.geometry.transform.hflip(features) if flipped else features for features, flipped in zip(depth_feature, mats_dict['flipped'])])
+
+        if depth_oracle is not None:
+            b, c, h, w = depth.shape
+            fg_mask = (torch.max(depth_oracle, dim=1).values > 0.0).view(-1)
+            depth_flattened = depth.permute(0, 2, 3, 1).contiguous().view(-1, c)
+            depth_oracle_flattened = depth_oracle.permute(0, 2, 3, 1).contiguous().view(-1, c)
+            depth_updated = depth_flattened
+            depth_updated[fg_mask] = depth_oracle_flattened[fg_mask]
+            depth_updated = depth_updated.view(b, h, w, c).permute(0, 3, 1, 2)
+
+            img_feat_with_depth = depth_updated.unsqueeze(
+                1) * depth_feature[:, self.depth_channels:(
+                    self.depth_channels + self.output_channels)].unsqueeze(2)
+
+        else:
+            img_feat_with_depth = depth.unsqueeze(
+                1) * depth_feature[:, self.depth_channels:(
+                    self.depth_channels + self.output_channels)].unsqueeze(2)
+
+        img_feat_with_depth = self._forward_voxel_net(img_feat_with_depth)
+
+        img_feat_with_depth = img_feat_with_depth.reshape(
+            batch_size,
+            num_cams,
+            img_feat_with_depth.shape[1],
+            img_feat_with_depth.shape[2],
+            img_feat_with_depth.shape[3],
+            img_feat_with_depth.shape[4],
+        )
+        geom_xyz = self.get_geometry(
+            mats_dict['sensor2ego_mats'][:, sweep_index, ...],
+            mats_dict['intrin_mats'][:, sweep_index, ...],
+            mats_dict.get('bda_mat', None),
+        )
+        img_feat_with_depth = img_feat_with_depth.permute(0, 1, 3, 4, 5, 2)
+        geom_xyz = ((geom_xyz - (self.voxel_coord - self.voxel_size / 2.0)) /
+                    self.voxel_size).int()
         feature_map = img_feat_with_depth.mean(dim=2).mean(dim=1)
-
         if is_return_depth:
             return feature_map.contiguous(), depth
         return feature_map.contiguous()
-
-    def forward(self, sweep_imgs, mats_dict, depth_oracle, timestamps=None, is_return_depth=False):
-        """Forward function."""
-        batch_size, num_sweeps, num_cams, num_channels, img_height, img_width = sweep_imgs.shape
-
-        key_frame_res = self._forward_single_sweep(
-            0,
-            sweep_imgs[:, 0:1, ...],
-            mats_dict,
-            depth_oracle,
-            is_return_depth=is_return_depth)
-
-        if num_sweeps == 1:
-            return key_frame_res
-
-        key_frame_feature = key_frame_res[0] if is_return_depth else key_frame_res
-        ret_feature_list = [key_frame_feature]
-        for sweep_index in range(1, num_sweeps):
-            with torch.no_grad():
-                feature_map = self._forward_single_sweep(
-                    sweep_index,
-                    sweep_imgs[:, sweep_index:sweep_index + 1, ...],
-                    mats_dict,
-                    depth_oracle,
-                    is_return_depth=False)
-                ret_feature_list.append(feature_map)
-
-        if is_return_depth:
-            return torch.cat(ret_feature_list, 1), key_frame_res[1]
-        else:
-            return torch.cat(ret_feature_list, 1)
-
 
     def forward(self,
                 sweep_imgs,
